cd49a0ac6b1b79baf9bcae0237c80357430e95b8
IsaacLezametaMinaya <lezametadavid@gmail.com>
2026-03-13 11:57:31 +0100

    El Recaudador pW

--- Arrays/src/Entrenamiento/El_Recaudador.java
+++ Arrays/src/Entrenamiento/El_Recaudador.java
@@ -5,7 +5,25 @@ import java.util.Scanner;
 public class El_Recaudador {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        int [] precios = new int[5];
+        int precio;
+        int total = 0;
 
+        for(int i = 0; i <= precios.length - 1; i ++) {
+            System.out.println("Introduzaca el precio del producto " + (i + 1) + " : ");
+            precio = sc.nextInt();
+            precios [i] = precio;
+        }
 
+        System.out.println("---- Lista de precios ----");
+        for (int i = 0; i <= precios.length - 1; i++){
+            System.out.print(precios[i] + " ");
+        }
+
+        for(int i = 0; i <= precios.length - 1; i++){
+            total+= precios[i];
+        }
+        System.out.println("\n" + "---- Total ----");
+        System.out.println("El total es: " + total);
     }
 }
